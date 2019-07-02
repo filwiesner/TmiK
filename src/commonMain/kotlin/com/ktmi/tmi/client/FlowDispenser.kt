ad2ac7da2cccb4fcfdbedea4b4ce4192dba82908
Filip <filip5871@gmail.com>
2019-07-02 02:05:14 +0200

    Synchronized access to callback list

--- src/commonMain/kotlin/com/ktmi/tmi/client/FlowDispenser.kt
+++ src/commonMain/kotlin/com/ktmi/tmi/client/FlowDispenser.kt
@@ -9,7 +9,6 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import kotlin.coroutines.CoroutineContext
 import kotlin.jvm.Synchronized
 
@@ -26,6 +25,11 @@ private data class FlowCallback <T> (
  */
 class FinishedSourceException : Exception("Source channel has finished sending messages")
 
+//private sealed class DispenserActor
+//private class Add<T>(val callback: FlowCallback<T>) : DispenserActor()
+//private class Remove<T>(val callback: FlowCallback<T>) : DispenserActor()
+//private class NotifyAll<T>(val value: T) : DispenserActor()
+
 /**
  * Turns [Channel] into [Flow]. Holds [FlowCallback]s that will be notified of new value or channel closing.
  * Consumes events in given [Flow]
@@ -36,35 +40,28 @@ class FlowDispenser <T> (
     val inChannel: ReceiveChannel<T>,
     override val coroutineContext: CoroutineContext
 ) : CoroutineScope {
-    private val flows = mutableListOf<FlowCallback<T>>()
+    private val _flows = mutableListOf<FlowCallback<T>>()
     private var job: Job? = null
 
-    private val mutex = Mutex()
+    @Synchronized
+    private fun changeState(block: (MutableList<FlowCallback<T>>) -> Unit) = block(_flows)
 
     /** When called, [FlowDispenser] will start to consume [inChannel] events */
     fun initialize() {
         if (running) return
         job = launch {
             try {
-                for (value in inChannel) {
-                    mutex.withLock {
-                        flows.forEach {
-                            it.onNextValue(value)
-                        }
+                for (value in inChannel)
+                    changeState { flows ->
+                        flows.forEach { it.onNextValue(value) }
                     }
-                }
             } catch (t: Throwable) {
-                flows.forEach { it.onClose(t) }
+                changeState { flows ->
+                    flows.forEach { it.onClose(t) }
+                }
             }
         }
     }
-
-    @Synchronized
-    private fun addCallback(callback: FlowCallback<T>) = flows.add(callback)
-
-    @Synchronized
-    private fun removeCallback(callback: FlowCallback<T>) = flows.remove(callback)
-
 
     /** Creates one [Flow] listening to [inChannel] */
     fun requestFlow() = callbackFlow<T> {
@@ -72,14 +69,15 @@ class FlowDispenser <T> (
             { offer(it) },
             { close(it ?: FinishedSourceException()) }
         )
-
-        mutex.withLock { flows.add(callback) }
+        changeState { flows ->
+            flows.add(callback)
+        }
 
         if (inChannel.isClosedForReceive)
             channel.close()
 
-        awaitClose { launch {
-            mutex.withLock { flows.remove(callback) }
+        awaitClose { changeState { flows ->
+            flows.remove(callback)
         } }
     }
 
@@ -94,5 +92,7 @@ class FlowDispenser <T> (
     /** clears all requested [Flow]s.
      * @see requestFlow
      */
-    fun clear() = flows.clear()
+    fun clear() = changeState { flows ->
+        flows.clear()
+    }
 }
